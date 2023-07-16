97de3f24d5312f3b5b84ccf30b7b6190bc0898d7
KenYuChang <justin0915justin0915@gmail.com>
2023-07-16 19:43:29 +0800

    view for product list

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,12 +15,12 @@ export default {
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
     console.log(token);
     // token加入headers
-    this.$http.defaults.headers.common.Authorization = token;
-    const api = `${process.env.VUE_APP_API}api/currentuser`;
-    this.$http.get(api, this.user)
-      .then((res) => {
-        console.log(res);
-      });
+    // this.$http.defaults.headers.common.Authorization = token;
+    // const api = `${process.env.VUE_APP_API}api/currentuser`;
+    // this.$http.get(api, this.user)
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
   },
 };
 </script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -11,17 +11,18 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>分類</td>
-        <td>標題</td>
+      <tr v-for="item in products" :key="item.id">
+        <td>{{ item.Category.name }}</td>
+        <td>{{ item.name }}</td>
         <td class="text-right">
-          200
+          {{ item.origin_price }}
         </td>
         <td class="text-right">
-          100
+          {{ item.price }}
         </td>
         <td>
-          <span class="text-success">啟用</span>
+          <span class="text-success" v-if="item.is_enabled">啟用</span>
+          <span class="text-muted" v-else>未啟用</span>
         </td>
         <td>
           <div class="btn-group">
@@ -45,9 +46,8 @@ export default {
       const api = `${process.env.VUE_APP_API}api/admin/products`;
       this.$http.get(api)
         .then((res) => {
-          if (res.data.success) {
-            console.log(res.data);
-          }
+          console.log(res.data);
+          this.products = res.data.products.products;
         });
     },
   },
